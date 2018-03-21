31817d0686f752af04cd7de0162615d500736346
LV <mamboer@live.com>
2018-03-22 00:06:09 +0800

    finish homepage

--- themes/booty/source/js/mods/common.js
+++ themes/booty/source/js/mods/common.js
@@ -1,40 +1,51 @@
 /* global App */
 (function($, App) {
 
-    $.extend(App, {
-        $win: $(window),
-        $doc: $(document),
-        $body: $('body'),
-        $htmlBody: $('html, body'),
-        $fullPages: $('.fullscreen')
-    });
+  $.extend(App, {
+    $win: $(window),
+    $doc: $(document),
+    $body: $('body'),
+    $htmlBody: $('html, body'),
+    $fullPages: $('.fullscreen')
+  });
 
-    function onResize() {
-        App.winH = App.$win.height();
-        App.winW = App.$win.width();
-        App.$fullPages.height(App.winH);
+  function onResize() {
+    App.winH = App.$win.height();
+    App.winW = App.$win.width();
+    App.$fullPages.height(App.winH);
+  }
+
+  var loader = {
+    destroy: function() {
+      var $dom = $("#maskLoader").delay(500).fadeOut('slow', function() {
+        $dom.remove();
+        App.$win.trigger('loaderGone');
+      });
     }
+  };
 
-    var loader = {
-        destroy: function() {
-            var $dom = $("#loader").delay(500).fadeOut('slow', function() {
-                $dom.remove();
-                App.$win.trigger('loaderGone');
-            });
-        }
-    };
+  var menu = {
+    init: function () {
+      $('#navBurger').on('click', function () {
+        this.classList.toggle('is-active');
+        document.querySelector('#navMenu').classList.toggle('is-active');
+        return false;
+      });
+    }
+  };
 
-    App.$win.on('resize', function() {
-        onResize();
-        App.$win.trigger('siteResized');
-    }).on('load', function() {
-        onResize();
-        loader.destroy();
-        App.$win.trigger('siteLoaded');
-    }).on('loaderGone', function() {
-        App.$htmlBody.removeClass('ovf-hidden');
-    });
-
+  App.$win.on('resize', function() {
     onResize();
+    App.$win.trigger('siteResized');
+  }).on('load', function() {
+    onResize();
+    loader.destroy();
+    menu.init();
+    App.$win.trigger('siteLoaded');
+  }).on('loaderGone', function() {
+    App.$htmlBody.removeClass('ovf-hidden');
+  });
+
+  onResize();
 
 })(jQuery, App);
