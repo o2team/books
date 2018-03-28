4fa1bcea0f3d7b2d82bb82c578dfc2830682bc7a
LV <mamboer@live.com>
2018-03-28 17:46:01 +0800

    chore: init src

---
+++ README.md
@@ -0,0 +1,36 @@
+## 逐帧动画 DEMO - 京东年货到家
+
+这是掘金小册《大厂 UI 开发实战手册》中『8.动效开发：逐帧动画』章节的手把手教导案例DEMO。
+
+## 项目运行
+
+项目是基于[ELF](https://github.com/o2team/elf)HTML5 构建工具进行开发的。
+
+因此，运行此项目需要以下步骤：
+
+1. `git clone` 本项目
+
+```
+git clone git@github.com:xtutor/ui-dev-handbook.git
+```
+
+2. 安装 [ELF](https://github.com/o2team/elf)
+
+按照[官方指南](https://github.com/o2team/elf)进行安装。
+
+3. 拷贝[daojia](./daojia)项目内容
+
+拷贝[daojia](./daojia)项目放入工作目录
+
+4. 进入新拷贝的项目并安装依赖
+
+```
+# 安装依赖
+$ cd daojia && npm install
+```
+
+5. 运行项目
+
+```
+$ elf start
+```
---
+++ daojia/.elf.config.js
@@ -0,0 +1,48 @@
+/**
+ * ELF 配置文件
+ *
+ * 详细说明：https://github.com/o2team/elf/blob/master/doc/CONFIGURATION.md
+ */
+module.exports = {
+  devPort: '8000', // dev server 运行的端口
+  designLayoutWidth: 640, // 设计稿的宽度 | 默认750，如果开启 Zoom 则直接按照设计稿宽度和屏幕宽度进行缩放
+  designLayoutHeight: 1028, // 设计稿的高度 | 默认1206，如果开启 Zoom 则直接按照设计稿高度和屏幕高度进行缩放
+  baseZoomRuler: 'width', // Zoom 缩放的基准 | 默认为 'width'，以屏幕的宽度进行缩放
+  baseSize: 10, // 计算 rem 的基数，通常不用修改
+  enableREM: true, // 是否用 rem 做适配
+  enableZoom: true, // 是否用 zoom 做适配
+
+  /**
+   * webpack base config
+   */
+  entry: 'src/js/main.js',
+  output: {
+    path: 'dist',
+    publicPath: './',
+    filename: 'js/bundle.js'
+  },
+  outputCss: 'css/app.css',
+  outputCssPublicPath: '../',
+  imgLoaderQuery: {
+    limit: 1000,
+    name: 'img/[name].[ext]'
+  },
+  audioLoaderQuery: {
+    name: 'plugin/[name].[ext]'
+  },
+  imgToBase64Dir: /src\/img-base64/,
+
+  /**
+   * webpack development config
+   *
+   * 只作用于 development 模式，会覆盖 base config 中相同的配置项
+   **/
+  DEVELOPMENT: {},
+
+  /**
+   * webpack production config
+   *
+   * 只作用于 production 模式，会覆盖 base config 中相同的配置项
+   **/
+  PRODUCTION: {}
+};
